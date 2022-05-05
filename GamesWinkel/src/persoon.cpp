3994559ce9c1490865eee69e799962ba674fdd5e
UnknownZandbak <rjwillard01@gmail.com>
2022-05-05 16:34:34 +0200

    changed output of buy and sell method
    more descriptive output of with person buys what from who and if this transaction was a success or not

--- GamesWinkel/src/persoon.cpp
+++ GamesWinkel/src/persoon.cpp
@@ -15,53 +15,67 @@ Person::~Person()
 
 void Person::buy(Game game)
 {
+    cout << name << " koopt: " << game.getTitle() << endl;
+
     // check of het persoon genoeg geld heeft om de game te kopen
     if (game.getOriginalPrice() <= budget){
         // check of het de game al in bezit heeft
         for (Game x: games_in_bezit){
             if (x.getTitle() == game.getTitle()){
-                cout << "Transactie Niet Gelukt" << endl;
-                return;
+                goto transaction_failed;
             }
         }
         // haal het geld van het budget af en stop de game in zijn bezit
         budget = budget - game.CalculateCurrentPrice();
         games_in_bezit.push_back(game);
 
-        cout << "Transactie Gelukt" << endl;
-        return;
+        goto transaction_succes;
     } 
     else {
-        cout << "Transactie Niet Gelukt" << endl;
-        return;
+        goto transaction_failed;
     }
+
+    transaction_succes: 
+        cout << "Transaction Succes" << endl;
+        goto done;
+    transaction_failed: 
+        cout << "Transaction Failed" << endl;
+    done:
+    cout << "-----------" << endl;
+    
 }
 
 void Person::sell(Game game, Person persoon)
 {   
+    cout << name << " verkoopt " << game.getTitle() << " aan: " << persoon.name << endl;
+
     // check of het persoon genoeg geld heeft om de game te kopen
     if (game.getOriginalPrice() <= persoon.budget){
         // check of het de game al in bezit heeft
         for (Game x: persoon.games_in_bezit){
             if (x.getTitle() == game.getTitle()){
-                cout << "Transactie Niet Gelukt" << endl;
-                return;
+                goto transaction_failed;
             }
         }
         // TODO Zorg ervoor dat de game bij de verkoper uit zijn lijst van games gaat
         budget = budget + game.CalculateCurrentPrice();
         persoon.budget = persoon.budget - game.CalculateCurrentPrice();
         
+        // this->games_in_bezit.
         persoon.games_in_bezit.push_back(game);
-
-        cout << "Transactie Niet Gelukt" << endl;
-        return;
+        goto transaction_succes;
     } 
     else {
-        cout << "Transactie Niet Gelukt" << endl;
-        return;
-
+        goto transaction_failed;
     }
+    
+    transaction_succes: 
+        cout << "Transaction Succes" << endl;
+        goto done;
+    transaction_failed: 
+        cout << "Transaction Failed" << endl;
+    done:
+    cout << "-----------" << endl;
 }
 
 vector<Game> Person::getGames()
