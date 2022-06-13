33e1489c16f3f67eb9160a371fe7831b8cbae5c1
UnknownZandbak <rjwillar01@gmail.com>
2022-06-13 14:17:16 +0200

    Inheritance

--- Dijkstra/src/include/stap.hpp
+++ Dijkstra/src/include/stap.hpp
@@ -3,14 +3,29 @@
 using std::string;
 
 class Stap {
-private:
+protected:
     string src;
     string dest;
     int weight;
-public:
 };
 
 class Vlucht: Stap{
 public:
+    void displayStap(){
+        cout << "Vlugt: " << src << "-->" << dest << "\tCost: " << weight << endl;
+    }
+};
 
+class Rit: Stap{
+public:
+    void displayStap(){
+        cout << "Rit: " << src << "-->" << dest << "\tCost: " << weight << endl;
+    }
+};
+
+class Treinrit: Stap{
+public:
+    void displayStap(){
+        cout << "Treinrit: " << src << "-->" << dest << "\tCost: " << weight << endl;
+    }
 };
